5696d318ca1751dedc6a69f20c5c41b309265616
Niseme <71128250+Niseme@users.noreply.github.com>
2021-11-22 15:52:46 +0200

    codewars-switch-case

---
+++ isspresti/switch-case.js
@@ -0,0 +1,48 @@
+//Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+//function evalObject(value){
+// var result = 0;
+// switch (value.operation) {
+//     case '+': result = value.a + value.b;
+//     case '-': result = value.a - value.b;
+//     case '/': result = value.a / value.b;
+//     case '*': result = value.a * value.b;
+//     case '%': result = value.a % value.b;
+//     case '^': result = Math.pow(value.a, value.b);
+// }
+// return result;
+// }
+
+function evalObject(value) {
+    var result = 0;
+    switch (value.operation) {
+        case '+': result = value.a + value.b;
+            break;
+        case '-': result = value.a - value.b; break;
+        case '/': result = value.a / value.b; break;
+        case '*': result = value.a * value.b; break;
+        case '%': result = value.a % value.b; break;
+        case '^': result = Math.pow(value.a, value.b); break;
+    }
+    return result;
+}
+
+describe('Fixed Tests', () => {
+    test("1+1", () => {
+        expect(evalObject({ a: 1, b: 1, operation: '+' })).toBe(2)
+    })
+    test("1+1", () => {
+        expect(evalObject({ a: 1, b: 1, operation: '-' })).toBe(0)
+    })
+    test("1+1", () => {
+        expect(evalObject({ a: 1, b: 1, operation: '*' })).toBe(1)
+    })
+    test("1+1", () => {
+        expect(evalObject({ a: 1, b: 1, operation: '%' })).toBe(0)
+    })
+    test("1+1", () => {
+        expect(evalObject({ a: 1, b: 1, operation: '^' })).toBe(1)
+    })
+})
+
+
+
